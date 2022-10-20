67a23b5d2a14b98d523e972a4ff343d7d86ec6fa
Denis Toritsyn <iadtden@gmail.com>
2022-10-20 20:27:31 +0500

    feat: task7, add a few attachments to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -41,7 +41,7 @@ class QuestionsController < ApplicationController
   private
 
   def set_question
-    @question = Question.find(params[:id])
+    @question = Question.with_attached_files.find(params[:id])
   end
 
   def set_user
@@ -49,6 +49,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body, :file)
+    params.require(:question).permit(:title, :body, files: [])
   end
 end
