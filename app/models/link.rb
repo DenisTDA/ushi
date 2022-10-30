e13730da012f2095d8fe30a596013322c89dd259
Denis Toritsyn <iadtden@gmail.com>
2022-10-30 17:54:10 +0500

    feat: task8, add validation for url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,11 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
-  validates :url, :name, presence: true
+  validates :url, :name, presence: true 
+  #validates :url, http_url: true
+  validates_format_of :url, with: URI::regexp(%w(http https))
+
+  def gist?
+    self.url.start_with?('https://gist.github.com/')
+  end
 end
--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -7,19 +7,68 @@ feature 'User can add links to answer', "
 " do
   given(:user) { create(:user) }
   given!(:question) { create(:question, author: user) }
-  given(:gist_url) { 'https://gist.github.com/DenisTDA/f08301933493bd34831712c35896cfab' }
-
-  scenario 'User adds links when create an answer', js: true do
+  
+  before do
     sign_in(user)
     visit question_path(question)
+  end
 
-    fill_in 'Your answer', with: 'Answer text text text'
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+  describe 'User adds' do
+    given(:simple_link) { 'https://google.com' }
+    given(:another_link) { 'https://yandex.ru' }
+
+    before do
+      fill_in 'Your answer', with: 'text text text'
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: simple_link
+    end
+
+    scenario 'link when give an answer', js: true do
+      click_on 'Answer'
+
+      within ".answers" do
+        expect(page).to have_link 'Google', href: simple_link
+      end
+    end
+
+    scenario 'two links when give an answer', js: true do
+      click_on 'add link'
+
+      all(:field, 'Link name').last.fill_in with: 'Yandex'
+      all(:field, 'Url').last.fill_in with: another_link
+
+      click_on 'Answer'
+      save_and_open_page
+
+      within ".answers" do
+        expect(page).to have_link 'Google', href: simple_link
+        expect(page).to have_link 'Yandex', href: another_link
+      end
+    end
+  end
+
+  scenario 'User adds link with incorrect URL when give an answer', js: true do
+    fill_in 'Your answer', with: 'text text text'
+    fill_in 'Link name', with: 'My Gist'
+    fill_in 'Url', with: 'wrongURL'
+
     click_on 'Answer'
 
-    within '.answers' do
-      expect(page).to have_link 'My gist', href: gist_url
+    within ".answer-errors" do
+      expect(page).to have_text 'Links url is invalid'
+    end
+  end
+
+  scenario 'User adds link with gist when give an answer', js: true do
+    fill_in 'Your answer', with: 'text text text'
+    fill_in 'Link name', with: 'My Gist'
+    
+    fill_in 'Url', with: 'https://gist.github.com/DenisTDA/e0ad7f91d05dcf4f0cce8abdf6880be0'
+
+    click_on 'Answer'
+
+    within '.new-answer' do
+      expect(page).to have_content 'test gist for Ushi application'
     end
   end
 end
