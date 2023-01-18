1a6b92abcd30c5af0c4f4bce5f7007d21f766177
Denis Toritsyn <iadtden@gmail.com>
2023-01-19 02:00:11 +0500

    feat: task13, add question api with comment, files, links

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,9 +1,7 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at, :updated_at, :short_title
-  has_many :answers
-  belongs_to :author
+  attributes :id, :title, :body, :created_at, :updated_at
 
-  def short_title
-    object.title.truncate(7)
-  end
+  has_many :comments
+  has_many :links
+  has_many :files, serializer: FileSerializer
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Profiles API', type: :request do
+describe 'Questions API', type: :request do
   let(:headers) { { "CONTENT_TYPE" => "application/json", 
                       "ACCEPT" => "application/json" } } 
 
@@ -60,6 +60,77 @@ describe 'Profiles API', type: :request do
             expect(answer_response[attr]).to eq answer.send(attr).as_json 
           end
         end
+      end
+
+    end
+  end
+
+  describe 'GET /api/v1/question/id' do
+    let!(:question) { create(:question) }
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get "/api/v1/questions/#{question.id}", headers:headers
+        expect(response.status).to eq 401 
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get "/api/v1/questions/#{question.id}", params: { access_token: '1234' }, headers:headers
+        expect(response.status).to eq 401 
+      end
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let(:question_response) { json['question'] }
+      let(:file1) { Rack::Test::UploadedFile.new(Rails.root.join('spec/rails_helper.rb')) }
+      let(:file2) { Rack::Test::UploadedFile.new(Rails.root.join('spec/spec_helper.rb')) }
+      let!(:comments) { create_list(:comment, 3, commentable: question, user: create(:user)) }
+      let!(:link1) { create(:link, name: 'Google', url: 'http://gogle.com', linkable: question) }
+      let!(:link2) { create(:link, name: 'E1', linkable: question) }
+
+      before do
+        question.files.attach(file1)
+        question.files.attach(file2)
+        get "/api/v1/questions/#{ question.id }", params: { access_token: access_token.token }, headers:headers 
+      end
+
+      it 'return 200 status' do
+        expect(response).to be_successful 
+      end
+ 
+      it 'retun all public fields' do
+        %w[id title body created_at updated_at].each do |attr|
+          expect(question_response[attr]).to eq question.send(attr).as_json
+        end
+      end
+
+      it 'return list of links' do
+        expect(question_response['links'].size).to eq 2
+      end
+
+      it 'return list of comments' do
+        expect(question_response['comments'].size).to eq 3
+      end
+
+      it 'return list of attachments (files)' do
+        expect(question_response['files'].size).to eq 2
+      end
+
+      it 'contains link object' do
+        expect(question_response['links'].first['id']).to eq question.links.first.id
+      end
+
+      it 'contains comment object' do
+        expect(question_response['comments'].first['id']).to eq question.comments.first.id
+      end
+
+      it 'contains file object' do
+        expect(question_response['files'].first['id']).to eq question.files.first.id
+      end
+
+      it 'contains file url_path' do
+        expect(question_response['files'].first['url_path'])
+          .to eq Rails.application.routes.url_helpers.rails_blob_path(question.files.first, only_path: true)
       end
 
     end
