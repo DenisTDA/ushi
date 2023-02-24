cbbf9699f5deb2e2b970be3e5b3429f710f8ca22
Denis Toritsyn <iadtden@gmail.com>
2023-02-24 23:38:46 +0500

    change-3 manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,2 @@
 //= link_tree ../images
-//= link_directory ../../javascript/stylesheets .scss
 //= link_directory ../stylesheets .css
-//= link_directory ../stylesheets .css
-//= link ../../javascript/stylesheets/application.scss
-//= link ../../javascript/packs/application.js
