3d826a5f550710ec8c5eef8f24c8e618d7794c65
Denis Toritsyn <iadtden@gmail.com>
2023-02-25 12:18:27 +0500

    Edit-1 manifest.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "stylesheets/application"
+import * as bootstrap from 'bootstrap'
 import "bootstrap"
+import "../stylesheets/application"
 import Vote from "./Vote"
 window.Vote = Vote
 
