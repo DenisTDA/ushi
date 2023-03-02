b2cc14d04bdecce5a082c86f151a31a978b673cb
Denis Toritsyn <iadtden@gmail.com>
2023-03-02 20:59:33 +0500

    feat: task19, set radis for cache

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Ushi
                        request_specs: false,
                        controller_specs: true
     end
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 end
